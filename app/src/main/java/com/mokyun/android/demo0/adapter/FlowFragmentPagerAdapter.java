524c81b364cd5df545319b78809f0a45c5dac566
val hsu <vesper.val@gmail.com>
2018-11-01 23:12:55 +0800

    add FlowFragmentPagerAdapter

---
+++ app/src/main/java/com/mokyun/android/demo0/adapter/FlowFragmentPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.mokyun.android.demo0.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.view.ViewGroup;
+
+
+abstract public class FlowFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    FragmentManager mFm;
+
+    public FlowFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+        mFm = fm;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        Fragment f = (Fragment) super.instantiateItem(container, position);
+        String fragmentTag = f.getTag();
+
+        if (f != getItem(position)) {
+            FragmentTransaction ft = mFm.beginTransaction();
+            ft.remove(f);
+            f =getItem(position);
+            ft.add(container.getId(), f, fragmentTag);
+            ft.attach(f);
+            ft.commitAllowingStateLoss();
+        }
+        return f;
+    }
+}
